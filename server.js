47700fa75aaf4cf48959e46a63d227982ea864f7
sisakhanyan <sisakhanyan@themsteam.com>
2018-03-20 17:28:59 +0700

    Added MySql database

--- server.js
+++ server.js
@@ -3,51 +3,55 @@ const bodyParser = require('body-parser');
 const app = express()
 const router = express.Router()
 const port = 8081
-let posts = [
-    {
-        username: 'Jack',
-        id: 0,
-        title: 'Hello',
-        date: '2017-09-01T12:30:48',
-        updated: '2017-09-01T12:30:48',
-        description: 'This is the first image. Lorem ipsum dolor sit amet.',
-        imageurl: 'https://travelsize.me/wp-content/uploads/2018/03/1520268443_maxresdefault-480x240.jpg',
-    },
-    {
-        username: 'Anna',
-        id: 1,
-        title: '',
-        date: '2018-01-01T08:30:10',
-        updated: '2018-01-01T08:30:10',
-        description: 'This is the first image.',
-        imageurl: 'https://t3.ftcdn.net/jpg/01/33/92/36/240_F_133923669_az6zDEfhs2mw3Tkf3WxFjrKMnVCSyTRO.jpg',
-    },
-    {
-        username: 'Walter',
-        id: 2,
-        title: 'Nature',
-        date: '2018-02-13T22:40:38',
-        updated: '2018-02-13T22:40:38',
-        description: 'This is the first image. Lorem ipsum dolor sit amet.',
-        imageurl: 'http://ironxiron.co.uk/wp-content/uploads/2018/01/Nature-6-2-480x240.jpg',
+let mysql = require('mysql');
+let knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '12345',
+        database: 'instagram'
     }
-]
+});
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',
+    database: 'instagram'
+});
+
+let posts = knex('posts').select('*').then(response => posts = response);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next()
+})
+
 app.get('/api', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.json(posts)
 })
 
 app.post('/api/post', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    posts.push(req.body)
-    res.json(posts)
+    let post = Object.assign({}, req.body, { id: posts[posts.length - 1].id + 1 })
+    let sql = `INSERT INTO posts (username, id, title, date, updated, description, imageurl) VALUES ('${post.username}', ${post.id}, '${post.title}', '${post.date}', '${post.updated}', '${post.description}', '${post.imageurl}')`;
+    connection.query(sql, function (err, result) {
+        if (err) console.log("Something wrong");
+        console.log("1 post inserted");
+    });
+    res.json(post)
 })
 
-app.post(`/api/posts/:post_${posts.id}`, (req, res) => {
+app.post(`/api/posts/:post_id`, (req, res) => {
+    let sql = `UPDATE posts SET title = '${req.body.title}', description = '${req.body.description}', updated = '${req.body.updated}' WHERE id = ${req.body.id}`;
+    connection.query(sql, function (err, result) {
+        if (err) console.log("Something wrong");
+        console.log("1 post updated");
+    })
     posts.forEach(post => {
         if (post.id === req.body.id) {
             return Object.assign(post, req.body)
@@ -58,7 +62,11 @@ app.post(`/api/posts/:post_${posts.id}`, (req, res) => {
 })
 
 app.delete('/api/posts/:post_id', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    let sql = `DELETE FROM posts WHERE id = ${req.body.id}`;
+    connection.query(sql, function (err) {
+        if (err) console.log("Something wrong");
+        console.log("1 post deleted");
+    })
     postsCopy = posts.filter((post) => post.id !== req.body.id);
     posts = postsCopy
     res.json(posts)
