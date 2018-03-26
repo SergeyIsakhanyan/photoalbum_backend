72ba929e70753ebf56b1652368742f474f71c144
sisakhanyan <sisakhanyan@themsteam.com>
2018-03-26 20:06:56 +0700

    Added authorization

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const cors = require('cors')
 const app = express()
 const router = express.Router()
 const port = 8081
@@ -20,30 +23,48 @@ let connection = mysql.createConnection({
     database: 'instagram'
 });
 
-let posts = knex('posts').select('*').then(response => posts = response);
+
+const postsTableName = 'posts'
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
     next()
 })
 
-app.get('/api', (req, res) => {
-    res.json(posts)
+const authCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://isakhanyan.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://photoalbum.com',
+    issuer: "https://isakhanyan.auth0.com/",
+    algorithms: ['RS256']
+});
+
+
+
+app.get('/api', authCheck, (req, res) => {
+    knex(postsTableName).select('*').then((response => res.json(response)))
 })
 
 app.post('/api/post', (req, res) => {
-    let post = Object.assign({}, req.body, { id: posts[posts.length - 1].id + 1 })
-    let sql = `INSERT INTO posts (username, id, title, date, updated, description, imageurl) VALUES ('${post.username}', ${post.id}, '${post.title}', '${post.date}', '${post.updated}', '${post.description}', '${post.imageurl}')`;
-    connection.query(sql, function (err, result) {
-        if (err) console.log("Something wrong");
-        console.log("1 post inserted");
-    });
-    res.json(post)
+    knex(postsTableName).insert(req.body, 'id')
+        .then(
+            function (result, err) {
+                if (err) console.log("Something wrong");
+                knex(postsTableName).where('id', result).select('*')
+                    .then(response => res.json(response[0]))
+            }
+        );
 })
 
 app.post(`/api/posts/:post_id`, (req, res) => {
@@ -51,14 +72,9 @@ app.post(`/api/posts/:post_id`, (req, res) => {
     connection.query(sql, function (err, result) {
         if (err) console.log("Something wrong");
         console.log("1 post updated");
+        knex(postsTableName).where('id', req.body.id).select('*')
+            .then(response => res.json(response[0]))
     })
-    posts.forEach(post => {
-        if (post.id === req.body.id) {
-            return Object.assign(post, req.body)
-        }
-        return post
-    })
-    res.json(posts)
 })
 
 app.delete('/api/posts/:post_id', (req, res) => {
@@ -66,10 +82,50 @@ app.delete('/api/posts/:post_id', (req, res) => {
     connection.query(sql, function (err) {
         if (err) console.log("Something wrong");
         console.log("1 post deleted");
+        knex(postsTableName).select('*')
+            .then(response => res.json(response))
     })
-    postsCopy = posts.filter((post) => post.id !== req.body.id);
-    posts = postsCopy
-    res.json(posts)
 });
+
+
+app.get('/api/myposts', authCheck, (req, res) => {
+    knex('myposts').select('*').then((response => res.json(response)))
+})
+
+app.post('/api/mypost', (req, res) => {
+    knex('myposts').insert(req.body, 'id')
+        .then(
+            function (result, err) {
+                if (err) console.log("Something wrong!");
+                knex('myposts').where('id', result).select('*')
+                    .then(response => res.json(response[0]))
+            }
+        );
+})
+
+app.post(`/api/myposts/:post_id`, (req, res) => {
+    let sql = `UPDATE myposts SET title = '${req.body.title}', description = '${req.body.description}', updated = '${req.body.updated}' WHERE id = ${req.body.id}`;
+    connection.query(sql, function (err) {
+        if (err) console.log("Something wrong");
+        console.log('err'+err);
+        console.log("1 post updated");
+        knex('myposts').where('id', req.body.id).select('*')
+            .then(response => res.json(response[0]))
+    })
+})
+
+app.delete('/api/myposts/:post_id', (req, res) => {
+    let sql = `DELETE FROM myposts WHERE id = ${req.body.id}`;
+    connection.query(sql, function (err) {
+        if (err) console.log(err);
+        console.log("1 post deleted");
+        knex('myposts').select('*')
+            .then(response => res.json(response))
+    })
+});
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
